e4a1f620c02afea124b9fced10dbd53f49de0ce1
Dimon162 <Dimdim_kz@mail.ru>
2016-03-22 10:51:20 +0600

    add operation

--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -16,5 +16,42 @@ namespace calculator
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(textBox1.Text);
+            double second = Convert.ToDouble(textBox2.Text);
+            double result = first * second;
+            label1.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(textBox1.Text);
+            double second = Convert.ToDouble(textBox2.Text);
+            double result = first / second;
+            label1.Text = result.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(textBox1.Text);
+            double second = Convert.ToDouble(textBox2.Text);
+            double result = first+second;
+            label1.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(textBox1.Text);
+            double second = Convert.ToDouble(textBox2.Text);
+            double result = first - second;
+            label1.Text = result.ToString();
+        }
     }
 }
